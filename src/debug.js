7d11aa97022ba935bfd5e8db4f61de1266e15bb9
ww24e018 <ww24e018@technikum-wien.at>
2025-06-06 19:09:04 +0200

    try intellij

--- src/debug.js
+++ src/debug.js
@@ -1,4 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.tprint(ns.getServer('foobar'));
+  //ns.tprint(ns.getServer('foobar'));
+  ns.tprint(ns.getPlayer().factions);
+  ns.tprint(ns.getPlayer().factions.includes('The Black Hand'));
+  ns.tprint(JSON.stringify(ns.getPlayer(),null,"\t"))
 }
